25f7e24b08fc94bc94d3a8173d9ff1761f14226d
Santiago Cuello <santy.sc@hotmail.com>
2017-12-01 22:44:46 -0300

    Defines a banch database

--- src/main/java/CasaLubetkin/Main.java
+++ src/main/java/CasaLubetkin/Main.java
@@ -13,9 +13,9 @@ public class Main {
 
 
     public static void main() {
-       System.out.println("Hola");
-                
+       System.out.println("database");
+
     }
-    
-          
+
+
 }
