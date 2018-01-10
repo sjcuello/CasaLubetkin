06c40c1cda13875e60969e368aaad3c1beacfe41
CristianGonzalez07 <1995.cgonzalez.07@gmail.com>
2018-01-10 05:35:04 -0300

    functions related to obtaining places have been added

--- src/main/java/CasaLubetkin/JDBCModels/Place.java
+++ src/main/java/CasaLubetkin/JDBCModels/Place.java
@@ -1,8 +1,9 @@
 package CasaLubetkin.JDBCModels;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.javalite.activejdbc.Base;
 import org.javalite.activejdbc.Model;
-import org.javalite.activejdbc.validation.UniquenessValidator;
 
 public class Place extends Model {
 
@@ -20,6 +21,8 @@ public class Place extends Model {
      * @param p is the name of the province of the city created
      * @pre c,d,p != null
      * @post. true is returned if the palce was created correctly otherwise
+     * @return true if the palce was created correctly otherwise
+     * false.
      * false.
      */
   public static boolean createPlace(String c, String d, String p){
@@ -30,4 +33,67 @@ public class Place extends Model {
     boolean res = pl.save();
     return res;
   }
+
+    /**
+     * Function that returns the id of a given place a locality.
+     * @param locality is the locality to consult.
+     * @return the id of a given place a locality.
+     */
+    public static int getIdByLocality(String locality) {
+        Place place = Place.findFirst("city",locality);
+        return (int) place.get("id");
+    }
+
+    /**
+     * Function that returns the id of a given place a department.
+     * @param department is the department to consult.
+     * @return the id of a given place a department.
+     */
+    public static int getIdByDepartment(String department) {
+        Place place = Place.findFirst("department",department);
+        return (int) place.get("id");
+    }
+    
+    /**
+     * Function that returns all the localities.
+     * @return a list with all the localities.
+     */
+    public static List<String> getAllLocalities(){
+        List<Place> places = Place.where("id > 0");
+        List<String> localities = new ArrayList<>();
+      for(Place place : places) {
+          localities.add(place.getString("city"));
+      }
+      return localities;
+    } 
+    /**
+     * Function that returns all the departments.
+     * @return a list with all the departments.
+     */
+    public static List<String> getAllDepartments(){
+        List<String> departments = new ArrayList<>();
+        List<Place> places = Place.where("id > 0");
+        String department = "";
+        for (Place place : places) {
+            department =  place.getString("department");
+            if(!departments.contains(department)){
+                departments.add(department);
+            }
+        }
+        return departments;
+    }
+    
+    /**
+     * function that returns all the locations of a department.
+     * @param department is the department to consult.
+     * @return a list with the ids of all the locations of a department.
+     */
+    public static List<Integer> getLocationsOfADepartment(String department){
+        List<Place> places = Place.where("department = ?",department);
+        List<Integer> localities = new ArrayList<>();
+        for (Place place : places) {
+                localities.add((Integer)place.get("id"));
+            }
+        return localities;
+    }
 }
